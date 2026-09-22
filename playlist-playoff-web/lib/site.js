37b72262091dbdf7bd7bb1cc5ee6202f97bebc80
lan-den11 <sllktiger111@gmail.com>
2026-09-21 21:08:42 -0500

    google search 5

--- playlist-playoff-web/lib/site.js
+++ playlist-playoff-web/lib/site.js
@@ -5,5 +5,5 @@ export const SITE_URL = (
   process.env.NEXT_PUBLIC_SITE_URL ||
   (process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : 'http://localhost:3000')
+    : 'https://playlistplayoff.onrender.com')
 ).replace(/\/$/, '');
